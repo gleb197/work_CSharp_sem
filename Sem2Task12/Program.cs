6aad2060d5306165a4baf51276f0d883f392c735
gleb <petrov.gp2017@yandex.ru>
2023-09-20 21:51:04 +0300

    Seminar2

---
+++ Sem2Task12/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает два чила и выводит кратно ли второе 
+//число первому. Если чило не кратно, то выводит остаток от деления
+
+Console.Write("Введите число ");
+int numA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число ");
+int numB = int.Parse(Console.ReadLine()??"0");
+if (numB % numA == 0)
+{
+    Console.WriteLine("Второе число кратно первому");
+}
+else
+{
+    Console.WriteLine("Второе число не кратно первому, остаток от деления "+(numB % numA));
+}
---
+++ Sem2Task16/Program.cs
@@ -0,0 +1,26 @@
+﻿//Напишите программу, которая принимает на вход два числа и проверяет
+//является ли первое число квадратом второго
+
+Console.Write("Введите число ");
+int numA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число ");
+int numB = int.Parse(Console.ReadLine()??"0");
+
+bool test1 = (numA == numB * numB);
+bool test2 = (numB == numA * numA);
+
+if (test1)
+{
+    Console.WriteLine("Первое число квадрат второго");
+}
+if (test2) 
+{
+    Console.WriteLine("Второе число квадрат первого");
+}
+if (!(test1 || test2))
+{
+    Console.WriteLine("Числа не квадраты друг друга");
+}
+
+
+
