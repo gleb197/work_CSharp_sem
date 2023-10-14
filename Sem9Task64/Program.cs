0286e2290c0e492eddf075bec51949c5dd8adfde
gleb <petrov.gp2017@yandex.ru>
2023-10-14 17:18:29 +0300

    Sem6

---
+++ Sem9Task64/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1
+
+
+//Ввод данных
+{
+Console.WriteLine(msg);
+int num = int.Parse(Console.ReadLine()??"0");
+return num;
+}
+
+if (n>0)
+return n==0;
+for (int i = n; i <0; i++)
+    Console.Write($" {i}");
+
+return n;
+
+//Печать резльтата
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+Console.Write("Введите число ");
+int num = int.Parse(Console.ReadLine()??"0");
+PrintResult(n);
---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+//Ввод данных
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num = int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+
+if (num>=arr.Length)
+    return 0;
+    return arr[i] = sum(arr, i+1);
+    
+
+
+//Печать резльтата
+void PrintResult(string msg)
+{
+    Console.WriteLine(msg);
+}
+
+
+
+
+Console.Write("Введите число M ");
+int numA = int.Parse(Console.ReadLine()??"0");
+Console.Write("Введите число N ");
+int numA = int.Parse(Console.ReadLine()??"0");
+
